0f889e4223a0b56b139875adc7c1aaca48a34ec1
Maya Yagan <maya2002yagan@gmail.com>
2025-10-10 23:52:57 +0300

    added header component to task4

---
+++ task4/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-gradient-to-r from-[#5F213E] to-[#914659] p-6 shadow-[0_4px_10px_rgba(95,33,62,0.3)]">
+      <h1 className="text-white text-4xl md:text-5xl font-bold text-center">
+        My Personal Blog
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
